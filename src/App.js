c7084225ad1e483ac3d48035323efb235438a67b
kim <kim_man@live.com>
2021-11-02 14:49:15 +1100

    got rid of warnings as theyre treated as errors when deploying!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React, { Fragment } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import "./App.css";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
-import Gear from "./components/pages/Gear";
-import Home from "./components/pages/Home";
-import Indoor from "./components/pages/Indoor";
-import Outdoor from "./components/pages/Outdoor";
-import Signup from "./components/pages/Signup";
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Gear from './components/pages/Gear';
+import Home from './components/pages/Home';
+import Indoor from './components/pages/Indoor';
+import Outdoor from './components/pages/Outdoor';
+import Signup from './components/pages/Signup';
 const App = () => {
   return (
     <Fragment>
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/indoor" component={Indoor} />
-          <Route path="/outdoor" component={Outdoor} />
-          <Route path="/gear" component={Gear} />
-          <Route path="/sign-up" component={Signup} />
+          <Route path='/' exact component={Home} />
+          <Route path='/indoor' component={Indoor} />
+          <Route path='/outdoor' component={Outdoor} />
+          <Route path='/gear' component={Gear} />
+          <Route path='/sign-up' component={Signup} />
         </Switch>
         <Footer />
       </Router>
--- src/components/CardsOutdoor.js
+++ src/components/CardsOutdoor.js
@@ -1,78 +1,77 @@
-import React from "react";
-import "./Cards.css";
-import CardItem from "./CardItem";
-import { Modal } from "./Modal";
+import React from 'react';
+import './Cards.css';
+import CardItem from './CardItem';
 
 const CardsOutdoor = ({ showModal, setShowModal }) => {
   return (
-    <div className="cards">
+    <div className='cards'>
       <h1>
         “The best part of climbing is when it all clicks and gravity ceases to
         exist.” – Chris Sharma
       </h1>
-      <div className="cards__container">
-        <div className="cards__wrapper">
-          <ul className="cards__items">
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
             <CardItem
-              src={require("../images/chrisSharmaWallpaper.jpeg").default}
-              text="Chris Sharma"
-              label="Legend"
-              path="/outdoor"
+              src={require('../images/chrisSharmaWallpaper.jpeg').default}
+              text='Chris Sharma'
+              label='Legend'
+              path='/outdoor'
             />
             <CardItem
-              src={require("../images/alex-honnold.jpeg").default}
-              text="camping on the mountain"
-              label="drawing"
-              path="/outdoor"
-            />
-          </ul>
-          <ul className="cards__items">
-            <CardItem
-              src={require("../images/abandonedTower.jpeg").default}
-              text="Abandoned power station tower turned rock climbing gym!"
-              label="adventure"
-              path="/outdoor"
-            />
-            <CardItem
-              src={require("../images/alex-honnold2.jpeg").default}
-              text="camping on the mountain"
-              label="High Ball"
-              path="/outdoor"
-            />
-            <CardItem
-              src={require("../images/alexMegos.jpeg").default}
-              text="Alex Megos"
-              label="Outdoor"
-              path="/outdoor"
+              src={require('../images/alex-honnold.jpeg').default}
+              text='camping on the mountain'
+              label='drawing'
+              path='/outdoor'
             />
           </ul>
-          <ul className="cards__items">
+          <ul className='cards__items'>
             <CardItem
-              src={require("../images/climbingLED.jpeg").default}
-              text="LED time lapse mountain"
-              label="LED"
-              path="/indoor"
+              src={require('../images/abandonedTower.jpeg').default}
+              text='Abandoned power station tower turned rock climbing gym!'
+              label='adventure'
+              path='/outdoor'
             />
             <CardItem
-              src={require("../images/Midtbo_Thors-Hammer_Cover.jpeg").default}
-              text="Magnus Midtbo"
-              label="Magnus"
-              path="/indoor"
+              src={require('../images/alex-honnold2.jpeg').default}
+              text='camping on the mountain'
+              label='High Ball'
+              path='/outdoor'
+            />
+            <CardItem
+              src={require('../images/alexMegos.jpeg').default}
+              text='Alex Megos'
+              label='Outdoor'
+              path='/outdoor'
             />
           </ul>
-          <ul className="cards__items">
+          <ul className='cards__items'>
             <CardItem
-              src={require("../images/geometricAesthetics.jpeg").default}
-              text="Geometric Aesthetics"
-              label="aesthetics"
-              path="/indoor"
-              key="hi"
+              src={require('../images/climbingLED.jpeg').default}
+              text='LED time lapse mountain'
+              label='LED'
+              path='/indoor'
             />
             <CardItem
-              src={require("../images/mapleBridgeOregon.jpeg").default}
-              text="Maple Bridge Oregon"
-              label="bridge"
-              path="/indoor"
+              src={require('../images/Midtbo_Thors-Hammer_Cover.jpeg').default}
+              text='Magnus Midtbo'
+              label='Magnus'
+              path='/indoor'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CardItem
+              src={require('../images/geometricAesthetics.jpeg').default}
+              text='Geometric Aesthetics'
+              label='aesthetics'
+              path='/indoor'
+              key='hi'
+            />
+            <CardItem
+              src={require('../images/mapleBridgeOregon.jpeg').default}
+              text='Maple Bridge Oregon'
+              label='bridge'
+              path='/indoor'
             />
           </ul>
         </div>
